8242cb1f6f0362ef3a32e2e579e509f5604db5ec
dingyu <dingyu@supremind.com>
2022-05-24 17:46:07 +0800

    init commit

---
+++ member/src/test/java/com/dy/dcktmall/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.dcktmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
